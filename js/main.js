49b5c0e3c729b3c66b39fb38e6f13ae775eb0558
arbaaz <mirzaarbaaz123@gmail.com>
2022-07-29 14:40:11 -0400

    refactor, add comments, change modalfrom div element to form element, add reset() to the form, add functionality that creates book object and adds it to myLibrary array

--- js/main.js
+++ js/main.js
@@ -30,13 +30,21 @@ function addBookToLibrary() {
     let author = document.querySelector('.author').value
     let pages = document.querySelector('.pages').value
     let read = document.querySelector('.check').checked
-    localStorage.setItem(`${title} by ${author}`, `${pages}, read: ${read}`)
 
+    // create Book object and add it to myLibrary array
+    myLibrary.push(new Book(title,author,pages,read))
+    
+    // save the book in localStorage
+    localStorage.setItem(`${title} by ${author}`, `pages: ${pages}, read: ${read}`)
+
+    // create book element to be displayed on the main page
     let book = document.createElement('p')
     book.textContent = title
 
-    document.querySelector('main').appendChild(book.textContent)
+    document.querySelector('main').appendChild(book)
 
+    // reset the form and hide the modal when the submit button is clicked
+    document.querySelector('.modal-box').reset()
     modal.style.display = "none"
 }
 
