cac923df5f0f9d785025ce84181fae5f28549d1f
arbaaz <mirzaarbaaz123@gmail.com>
2022-08-01 18:23:18 -0400

    minor adjustments

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ function Book(title,author,pages,read) {
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+    this.remove = function() {
+        return document.querySelector('section').remove()
     }
 }
 
@@ -31,20 +31,27 @@ function addBookToLibrary() {
     let pages = document.querySelector('.pages').value
     let read = document.querySelector('.check').checked
 
+    // create a book (card/section) where the book's info will be displayed
+    let book = document.createElement('section')
+    book.style.cssText = "padding: 10px; border: 3px solid black; border-radius: 17px;"
+
+    // create book element to be displayed in the book section
+    let bookInfo = document.createElement('p')
+    bookInfo.innerHTML = title
+
+    // append bookInfo to book
+    book.appendChild(bookInfo)
+
+    // append book to main
+    document.querySelector('main').appendChild(book)
+
     // create Book object and add it to myLibrary array
     myLibrary.push(new Book(title,author,pages,read))
     
     // save the book in localStorage
     localStorage.setItem(`${title} by ${author}`, `pages: ${pages}, read: ${read}`)
 
-    // create book element to be displayed on the main page
-    let book = document.createElement('p')
-    book.textContent = title
-
-    document.querySelector('main').appendChild(book)
-
     // reset the form and hide the modal when the submit button is clicked
     document.querySelector('.modal-box').reset()
     modal.style.display = "none"
 }
-
